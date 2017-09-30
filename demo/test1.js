0a3dd455d3c436aa2cff07aeb6bb6e41c3f6f7f6
chinanf-boy <865501259@qq.com>
2017-09-30 15:12:56 +0800

    test1 update

--- demo/test1.js
+++ demo/test1.js
@@ -5,6 +5,7 @@ const path = require('./path1')
 const test2 = require('./test2')
 
 const t = require('./try_new_NodePath')
+
 // /(require|from)([(\s]+)?[\S]*((\'|\")[)]?)/g
 // /(require|from)([(\s]+)?(\'|\")[\S]+((\'|\")([\)])?)/g
 
