35e3bb60fb3edb8037d1df8ffd051dd68d3304d7
chinanf-boy <865501259@qq.com>
2017-09-30 16:54:28 +0800

    requireNodePath show Path

--- NodePath/index.js
+++ NodePath/index.js
@@ -1,13 +1,41 @@
+const path = require('path')
+const fs = require('fs')
+
 const requireNodePath = require('./requireNodePath')
 
-const fromNodePath = require('./fromNodePath')
+// const fromNodePath = require('./fromNodePath')
 
 const getMatch = (data) => data.match(/(require|from)([(\s]+)?(\'|\")[\S]+((\'|\")([\)])?)/g);
 
+function isLocalFunc(fileAllPath) {
+     if(fs.existsSync(fileAllPath)){
+        return true
+     }else{
+         throw Error('no such file\n'+fileAllPath+'\n')
+     }
+}
 
 // 获得参数
-let getFile = process.argv[2]
-if(!getFile){
+let getFileName = process.argv[2]
+if(!getFileName){
     throw Error("there is no index file\nlike:\n"+">node NodePath.js filename.js\n")
 }
 let hostdir = process.argv[1]
+let addFrom = process.argv[3]
+let Path_requireNodePath = require.resolve('./requireNodePath')
+
+console.time('NodePath:time')
+
+if(addFrom === 'es5'){
+    //  
+}else{
+    let getFile = path.join(process.cwd(), getFileName)
+    if(isLocalFunc(getFile)){
+        let R_result = requireNodePath.requireNodePath( getFile, [ hostdir, Path_requireNodePath] )
+        // console.log(Path_requireNodePath)
+        console.log(R_result)
+    }
+}
+console.timeEnd('NodePath:time')
+// 
+
--- NodePath/requireNodePath.js
+++ NodePath/requireNodePath.js
@@ -1,24 +1,17 @@
-// let getFile = process.argv[2]
-
-// if(!getFile){
-//     throw Error("there is no index file\nlike:\n"+">node NodePath.js filename.js\n")
-// }
-
-
-
-// let hostdir = process.argv[1]
-
-function requireCache( hostdir, getFile ){
+function requireNodePath( getFile, missDir ){
     const file1 = require(getFile)
-    
-    console.time('NodePath:time')
     
     // console.log(require.cache);
     
     let R_results = {}
     
-    let results = Object.keys(require.cache).filter(x => x!==hostdir)
-    
+    let results;
+    results = Object.keys(require.cache).filter(x => 
+            {
+                return !missDir.find(value => value === x)
+            }
+        )
+                
     // console.log(results)
     
     results.forEach(x => {
@@ -28,8 +21,9 @@ function requireCache( hostdir, getFile ){
         }
     })
     
-    console.log(R_results)
-    console.timeEnd('NodePath:time')
+    return R_results
 }
 
-exports.requireCache = requireCache
+module.exports = {
+    requireNodePath
+}
